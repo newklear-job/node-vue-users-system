959c14876eb0981d4f441fc351c8e5eca0102a8e
newklear-job <petergerinv@gmail.com>
2020-12-28 00:21:55 +0200

    Basic vuex ts setup.

---
+++ vue/src/store/mutations.ts
@@ -0,0 +1,6 @@
+import { MutationTree } from "vuex";
+import { State } from "./state";
+
+export type Mutations<S = State> = {};
+
+export const mutations: MutationTree<State> & Mutations = {};
---
+++ vue/src/store/state.ts
@@ -0,0 +1,3 @@
+export const state = {};
+
+export type State = typeof state;
