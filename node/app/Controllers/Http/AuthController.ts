25bda904c6c09331be306680ac533178370af5ce
newklear-job <petergerinv@gmail.com>
2021-01-11 21:52:18 +0200

    Send token on successful registration.

--- node/app/Controllers/Http/AuthController.ts
+++ node/app/Controllers/Http/AuthController.ts
@@ -3,15 +3,18 @@ import StoreUserValidator from 'App/Validators/Users/StoreUserValidator'
 import User from 'App/Models/User'
 
 export default class AuthController {
-  public async register({ request, response }: HttpContextContract) {
+  public async register({ request, auth, response }: HttpContextContract) {
     const userData = await request.validate(StoreUserValidator)
-    const user = await User.create(userData)
-    return response.created(user)
+    await User.create(userData)
+    const token = await auth.use('api').attempt(userData.email, userData.password)
+
+    return response.created(token.toJSON())
   }
   public async login({ request, auth, response }: HttpContextContract) {
     const email = request.input('email')
     const password = request.input('password')
     const token = await auth.use('api').attempt(email, password)
+
     return response.ok(token.toJSON())
   }
 }
